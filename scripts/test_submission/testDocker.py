1550e37f8a64b60d2e1aa321996cce7fd746b73a
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2019-07-21 08:50:59 +0200

    Added saving of frames to testDocker

--- scripts/test_submission/testDocker.py
+++ scripts/test_submission/testDocker.py
@@ -12,6 +12,7 @@ import numpy as np
 from animalai.envs.gym.environment import AnimalAIEnv
 from animalai.envs.arena_config import ArenaConfig
 
+N_EPISODES = 30
 
 def main():
     # Load the agent from the submission
@@ -35,6 +36,7 @@ def main():
         docker_training=True,
     )
 
+    _remove_previous_frames()
     config_filepaths = sorted(glob.glob('/aaio/test/configs/*.yaml'))
     rewards = []
     elapsed_time = time.time()
@@ -44,22 +46,28 @@ def main():
     _summarize_rewards(rewards, config_filepaths, elapsed_time)
     print('SUCCESS')
 
-def evaluate_config(config_filepath, env, submitted_agent, n_episodes=30):
+def evaluate_config(config_filepath, env, submitted_agent, n_episodes=N_EPISODES):
     arena_config_in = ArenaConfig(config_filepath)
     env.reset(arenas_configurations=arena_config_in)
     obs, reward, done, info = env.step([0, 0])
 
     print('\t%s' % os.path.basename(config_filepath))
     rewards = []
+    episode_frames = []
     for k in range(n_episodes):
         _reset_agent(submitted_agent, arena_config_in)
         cumulated_reward = 0
+        #episode_frames = []
         try:
             for i in range(arena_config_in.arenas[0].t):
                 action = submitted_agent.step(obs, reward, done, info)
+                episode_frames.append(info['brain_info'].visual_observations[0][0])
                 obs, reward, done, info = env.step(action)
                 cumulated_reward += reward
                 if done:
+                    # _save_frames(episode_frames, config_filepath, k)
+                    for _ in range(3):
+                        episode_frames.append(np.ones((84, 84, 3), dtype=np.float32)*0.5)
                     _reset_agent(submitted_agent, arena_config_in)
                     break
         except Exception as e:
@@ -68,6 +76,7 @@ def evaluate_config(config_filepath, env, submitted_agent, n_episodes=30):
         print('Episode %i: %.2f' % (k, cumulated_reward))
         rewards.append(cumulated_reward)
         sys.stdout.flush()
+    _save_frames(episode_frames, config_filepath, None)
     return rewards
 
 def _reset_agent(agent, arena):
@@ -89,6 +98,22 @@ def _summarize_rewards(rewards, config_filepaths, elapsed_time):
     print('Mean reward: %.2f' % np.mean(rewards))
     with open('/aaio/test/summary.json', 'w') as f:
         json.dump(data, f)
+
+def _save_frames(episode_frames, config_filepath, episode_idx):
+    episode_frames = np.array(episode_frames)
+    episode_frames = (episode_frames*255).astype(np.uint8)
+    if episode_idx is not None:
+        filepath = os.path.join('/aaio/test/frames', '%s_%03d.npz' % (
+            os.path.splitext(os.path.basename(config_filepath))[0], episode_idx))
+    else:
+        filepath = os.path.join('/aaio/test/frames', '%s.npz' % (
+            os.path.splitext(os.path.basename(config_filepath))[0]))
+    np.savez(filepath, frame=episode_frames)
+
+def _remove_previous_frames():
+    filepaths = glob.glob('/aaio/test/frames/*.npz')
+    [os.remove(filepath) for filepath in filepaths]
+
 
 if __name__ == '__main__':
     main()
