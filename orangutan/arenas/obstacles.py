395bee6ba1cb8d4a4aeacc63bff10cef777c7a44
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2019-10-05 13:07:06 +0200

    Progress in the implementation of obstacles levels

--- orangutan/arenas/obstacles.py
+++ orangutan/arenas/obstacles.py
@@ -4,8 +4,7 @@ Functions for creating arenas for obstacles category
 import numpy as np
 from animalai.envs.arena_config import Vector3, RGB, Item, Arena, ArenaConfig
 
-from orangutan.arenas.geometry import get_angle_looking_center, get_random_position, normalize_angle, get_position_in_front_of_agent, get_random_position_close_to_center
-from orangutan.arenas.food import _create_agent_looking_center_at_random_position, _create_goal_in_front_of_agent
+from orangutan.arenas.utils import GRAY, PINK
 
 DEFAULT_TIME_LIMIT = 500
 DEFAULT_REWARD = 2
@@ -18,15 +17,26 @@ def create_arena_with_obstacles(t=DEFAULT_TIME_LIMIT):
     return arena
 
 def _add_rewards_to_arena(arena):
-    pass
+    # TODO: add more complex goals
+    for _ in range(DEFAULT_REWARD):
+        item = Item(name='GoodGoalMulti', sizes=[Vector3(*[1]*3)])
+        arena.items.append(item)
 
 def _add_obstacles_to_arena(arena):
-    for _ in range(5):
-        _add_random_box(arena)
-        _add_random_wooden_object(arena)
+    for _ in range(10):
+        _add_random_inmovable_object(arena)
+        _add_random_movable_object(arena)
 
 def _add_badgoals_to_arena(arena):
-    pass
+    for _ in range(5):
+        item = Item(name='BadGoal', sizes=[Vector3(*[1]*3)])
+        arena.items.append(item)
+
+def _add_random_movable_object(arena):
+    if np.random.randint(0, 2):
+        _add_random_box(arena)
+    else:
+        _add_random_wooden_object(arena)
 
 def _add_random_box(arena):
     movable_objects = ['Cardbox1', 'Cardbox2']
@@ -40,4 +50,20 @@ def _add_random_wooden_object(arena):
     name = str(np.random.choice(movable_objects))
     sizes = [Vector3(-1, -1, float(np.random.randint(3, 10)))]
     item = Item(name=name, sizes=sizes)
+    arena.items.append(item)
+
+def _add_random_inmovable_object(arena):
+    inmovable_objects = ['Wall', 'WallTransparent', 'Ramp', 'Cillinder', 'CillinderTransparent']
+    name = str(np.random.choice(inmovable_objects))
+    if name in ['Wall', 'Cillinder']:
+        colors = [GRAY]
+    elif name == 'Ramp':
+        colors = [PINK]
+    else:
+        colors = []
+    if 'Wall' in name or name == 'Ramp':
+        sizes = [Vector3(*np.random.randint(1, 10, 3).tolist())]
+    else:
+        sizes = [Vector3(*np.random.randint(3, 10, 3).tolist())]
+    item = Item(name=name, sizes=sizes, colors=colors)
     arena.items.append(item)
---
+++ orangutan/arenas/utils.py
@@ -0,0 +1,5 @@
+from animalai.envs.arena_config import RGB
+
+GRAY = RGB(153, 153, 153)
+PINK = RGB(255, 0, 255)
+BLUE = RGB(0, 0, 255)
