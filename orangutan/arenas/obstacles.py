2b24de603b18ce28e4c8f0053262afe26641a067
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2019-10-06 19:41:15 +0200

    Added goal inside cillinder

--- orangutan/arenas/obstacles.py
+++ orangutan/arenas/obstacles.py
@@ -10,9 +10,6 @@ from orangutan.arenas.geometry import detect_collisions, CollisionDetected
 DEFAULT_TIME_LIMIT = 500
 DEFAULT_REWARD = 2
 
-# TODO: verify that goals do not overlap
-# TODO: what happens when some information is missing
-# TODO: detect collisions with arena border
 # TODO: add empty platforms
 # TODO: add empty group of boxes
 # TODO: add goal inside tunnel
@@ -27,9 +24,10 @@ def create_arena_with_obstacles(t=DEFAULT_TIME_LIMIT):
 
 def _add_rewards_to_arena(arena):
     # TODO: add more complex goals
-    for _ in range(DEFAULT_REWARD*10):
+    for _ in range(DEFAULT_REWARD):
         _add_goal_on_top_of_platform(arena)
-        # _add_goal_on_top_of_box(arena)
+        _add_goal_on_top_of_box(arena)
+        _add_goal_inside_cillinder(arena)
         # _add_simple_goal(arena)
 
 def _add_simple_goal(arena):
@@ -37,11 +35,16 @@ def _add_simple_goal(arena):
     arena.items.append(item)
 
 def _add_goal_on_top_of_box(arena):
-    box = _create_random_box()
-    border_distance = np.sqrt(box.sizes[0].x*2 + box.sizes[0].z*2)
-    x, z = np.random.uniform(border_distance, 40 -border_distance, 2).tolist()
-    box.positions = [Vector3(x, 0, z)]
-    arena.items.append(box)
+    while 1:
+        try:
+            box = _create_random_box()
+            border_distance = np.max([box.sizes[0].x, box.sizes[0].z])*1.2
+            x, z = np.random.uniform(border_distance, 40 -border_distance, 2).tolist()
+            box.positions = [Vector3(x, 0, z)]
+            arena.items.append(box)
+            break
+        except CollisionDetected:
+            pass
     goal = Item(name='GoodGoalMulti', sizes=[Vector3(*[1]*3)], positions=[Vector3(x, box.sizes[0].y, z)])
     arena.items.append(goal)
 
@@ -49,7 +52,7 @@ def _add_goal_on_top_of_platform(arena):
     while 1:
         try:
             platform = _create_random_platform()
-            border_distance = np.max([platform.sizes[0].x, platform.sizes[0].z])
+            border_distance = np.max([platform.sizes[0].x, platform.sizes[0].z])*1.2
             x, z = np.random.uniform(border_distance, 40 -border_distance, 2).tolist()
             platform.positions = [Vector3(x, 0, z)]
             ramp = _create_ramp_for_platform(platform)
@@ -63,6 +66,19 @@ def _add_goal_on_top_of_platform(arena):
     goal = Item(name='GoodGoalMulti', sizes=[Vector3(*[1]*3)], positions=[Vector3(x, platform.sizes[0].y, z)])
     arena.items.append(goal)
 
+def _add_goal_inside_cillinder(arena):
+    while 1:
+        try:
+            cillinder = _create_random_cillinder()
+            border_distance = np.max([cillinder.sizes[0].x, cillinder.sizes[0].z])*1.2
+            x, z = np.random.uniform(border_distance, 40 -border_distance, 2).tolist()
+            cillinder.positions = [Vector3(x, 0, z)]
+            arena.items.append(cillinder)
+            break
+        except CollisionDetected:
+            pass
+    goal = Item(name='GoodGoalMulti', sizes=[Vector3(*[1]*3)], positions=[Vector3(x, 0, z)])
+    arena.items.append(goal)
 
 def _add_obstacles_to_arena(arena):
     for _ in range(10):
@@ -98,9 +114,9 @@ def _add_random_wooden_object(arena):
     arena.items.append(item)
 
 def _add_random_inmovable_object(arena):
-    inmovable_objects = ['Wall', 'WallTransparent', 'Ramp', 'Cillinder', 'CillinderTransparent']
+    inmovable_objects = ['Wall', 'WallTransparent', 'Ramp', 'CylinderTunnelTransparent', 'CylinderTunnel']
     name = str(np.random.choice(inmovable_objects))
-    if name in ['Wall', 'Cillinder']:
+    if name in ['Wall', 'CylinderTunnel']:
         colors = [GRAY]
     elif name == 'Ramp':
         colors = [PINK]
@@ -120,6 +136,17 @@ def _create_random_platform():
     y = float(np.random.randint(1, 3))
     sizes = [Vector3(x, y, z)]
     item = Item(name=name, sizes=sizes, colors=colors, rotations=[0])
+    return item
+
+def _create_random_cillinder():
+    inmovable_objects = ['CylinderTunnelTransparent', 'CylinderTunnel']
+    name = str(np.random.choice(inmovable_objects))
+    if name == 'CylinderTunnel':
+        colors = [GRAY]
+    else:
+        colors = []
+    sizes = [Vector3(*np.random.randint(3, 10, 3).tolist())]
+    item = Item(name=name, sizes=sizes, colors=colors)
     return item
 
 def _create_ramp_for_platform(platform, rotation=None):
