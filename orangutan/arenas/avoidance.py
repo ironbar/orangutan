04d372a747c593923c15942ffbfdd95c5550bef0
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2019-10-08 18:34:22 +0200

    avoidance create_arena_splitted_in_two

--- orangutan/arenas/avoidance.py
+++ orangutan/arenas/avoidance.py
@@ -14,7 +14,8 @@ from orangutan.arenas.obstacles import (
     _add_goal_on_top_of_platform,
     _add_center_blocking_wall,
     _add_random_box,
-    _add_random_wooden_object
+    _add_random_wooden_object,
+    _split_arena_in_two
 )
 
 DEFAULT_TIME_LIMIT = 500
@@ -139,5 +140,20 @@ def create_center_blocked_arena(t):
     for _ in range(np.random.randint(2, 6)):
         _add_random_box(arena)
     if np.random.randint(0, 2):
+        _add_random_wooden_object(arena)
+    return arena
+
+def create_arena_splitted_in_two(t):
+    arena = Arena(t=t, items=[])
+    _split_arena_in_two(arena)
+    for item in arena.items:
+        item.name = 'DeathZone'
+    _add_agent_to_arena(arena)
+    for _ in range(DEFAULT_REWARD):
+        _add_simple_goal(arena)
+    _add_badgoals_to_arena(arena, np.random.randint(2, 7))
+    for _ in range(np.random.randint(2, 4)):
+        _add_random_box(arena)
+    for _ in range(np.random.randint(1, 3)):
         _add_random_wooden_object(arena)
     return arena
