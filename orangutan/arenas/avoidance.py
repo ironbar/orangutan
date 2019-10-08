17c583a9df03737996898a10d3a63575b8a71c2b
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2019-10-08 18:31:10 +0200

    Progress in the creation of avoidance levels

--- orangutan/arenas/avoidance.py
+++ orangutan/arenas/avoidance.py
@@ -11,7 +11,10 @@ from orangutan.arenas.obstacles import (
     _add_badgoals_to_arena,
     _add_goal_inside_cillinder,
     _add_goal_on_top_of_box,
-    _add_goal_on_top_of_platform
+    _add_goal_on_top_of_platform,
+    _add_center_blocking_wall,
+    _add_random_box,
+    _add_random_wooden_object
 )
 
 DEFAULT_TIME_LIMIT = 500
@@ -21,6 +24,7 @@ def create_arena_with_obstacles_and_deathzones(t=DEFAULT_TIME_LIMIT):
     arena = Arena(t=t, items=[])
     _add_rewards_to_arena(arena)
     _add_zones_to_arena(arena, np.random.randint(2, 5))
+    _add_agent_to_arena(arena)
     _add_obstacles_to_arena(arena, np.random.randint(5, 10))
     _add_badgoals_to_arena(arena, np.random.randint(2, 7))
     return arena
@@ -106,3 +110,34 @@ def _create_random_zone(zone_types=None):
     sizes = [Vector3(*np.random.randint(2, 10, 3).tolist())]
     item = Item(name=name, sizes=sizes, rotations=[float(np.random.randint(0, 360))])
     return item
+
+def _add_agent_to_arena(arena):
+    while 1:
+        try:
+            x, z = np.random.uniform(1, 39, 2).tolist()
+            goal = Item(name='Agent', sizes=[Vector3(*[1]*3)])
+            goal.positions = [Vector3(x, 0, z)]
+            detect_collisions(goal, arena.items)
+            arena.items.append(goal)
+            break
+        except CollisionDetected:
+            pass
+
+"""
+More levels
+"""
+
+def create_center_blocked_arena(t):
+    arena = Arena(t=t, items=[])
+    _add_center_blocking_wall(arena)
+    for item in arena.items:
+        item.name = 'DeathZone'
+    _add_agent_to_arena(arena)
+    for _ in range(DEFAULT_REWARD):
+        _add_simple_goal(arena)
+    _add_badgoals_to_arena(arena, np.random.randint(2, 7))
+    for _ in range(np.random.randint(2, 6)):
+        _add_random_box(arena)
+    if np.random.randint(0, 2):
+        _add_random_wooden_object(arena)
+    return arena
