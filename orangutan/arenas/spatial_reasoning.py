4014dfc1c0ec990713691b7acd8415a9bf30e45a
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2019-10-12 10:29:38 +0200

    code refactorization

--- orangutan/arenas/spatial_reasoning.py
+++ orangutan/arenas/spatial_reasoning.py
@@ -34,35 +34,40 @@ def _get_pillars_positions(n_cells):
     return positions
 
 def _add_walls(arena, n_cells, wall_thickness):
-    centers = np.linspace(0, 40, n_cells, endpoint=False)[1:].tolist()
-    wall_length = (40 - n_cells*wall_thickness)/n_cells
-
     maze = Maze()
     maze = maze.generate(n_cells, n_cells)
     print(maze)
 
     for cell in maze.cells:
-        cell_x_idx = cell.x
-        cell_z_idx = cell.y
-
-        # horizontal walls
-        if cell_z_idx < n_cells - 1 and 's' in cell.walls:
-            sizes = [Vector3(wall_length, 5, wall_thickness)]
-            if not cell_x_idx:
-                x = centers[cell_x_idx] - wall_length/2 - wall_thickness/2
-            else:
-                x = centers[cell_x_idx - 1] + wall_length/2 + wall_thickness/2
-            positions = [Vector3(x, 0, centers[cell_z_idx])]
-            wall = Item(name='Wall', positions=positions, rotations=[0], sizes=sizes)
+        positions, sizes = _get_cell_walls_positions_and_sizes(
+            cell, wall_thickness, n_cells)
+        if positions:
+            wall = Item(name='Wall', positions=positions, rotations=[0]*len(positions), sizes=sizes)
             arena.items.append(wall)
 
-        # vertical walls
-        if cell_x_idx < n_cells - 1 and 'e' in cell.walls:
-            sizes = [Vector3(wall_thickness, 5, wall_length)]
-            if not cell_z_idx:
-                z = centers[cell_z_idx] - wall_length/2 - wall_thickness/2
-            else:
-                z = centers[cell_z_idx - 1] + wall_length/2 + wall_thickness/2
-            positions = [Vector3(centers[cell_x_idx], 0, z)]
-            wall = Item(name='Wall', positions=positions, rotations=[0], sizes=sizes)
-            arena.items.append(wall)
+def _get_cell_walls_positions_and_sizes(cell, wall_thickness, n_cells):
+    centers = np.linspace(0, 40, n_cells, endpoint=False)[1:].tolist()
+    wall_length = (40 - n_cells*wall_thickness)/n_cells
+
+    positions, sizes = [], []
+    cell_x_idx = cell.x
+    cell_z_idx = cell.y
+
+    # horizontal walls
+    if cell_z_idx < n_cells - 1 and 's' in cell.walls:
+        sizes.append(Vector3(wall_length, 5, wall_thickness))
+        if not cell_x_idx:
+            x = centers[cell_x_idx] - wall_length/2 - wall_thickness/2
+        else:
+            x = centers[cell_x_idx - 1] + wall_length/2 + wall_thickness/2
+        positions.append(Vector3(x, 0, centers[cell_z_idx]))
+
+    # vertical walls
+    if cell_x_idx < n_cells - 1 and 'e' in cell.walls:
+        sizes.append(Vector3(wall_thickness, 5, wall_length))
+        if not cell_z_idx:
+            z = centers[cell_z_idx] - wall_length/2 - wall_thickness/2
+        else:
+            z = centers[cell_z_idx - 1] + wall_length/2 + wall_thickness/2
+        positions.append(Vector3(centers[cell_x_idx], 0, z))
+    return positions, sizes
