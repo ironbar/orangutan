f43375fa74e716bb82f775b016f44bfe6784e3f3
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2019-10-24 18:49:46 +0200

    Added option to the MapEnv to modify the map side

--- orangutan/env.py
+++ orangutan/env.py
@@ -42,12 +42,13 @@ class MapEnv(object):
 
     It will only work correctly if using a single arena on each environment
     '''
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, map_side=60, **kwargs):
         '''
         Check UnityEnvironment parameters
         '''
         self._env = EnvWrapper(*args, **kwargs)
         self._arena_map = None
+        self._map_side = map_side
 
     def __getattr__(self, attr):
         if attr in self.__dict__:
@@ -70,7 +71,7 @@ class MapEnv(object):
         return ret
 
     def _add_map_to_brain_info(self, ret):
-        heatmap = self._arena_map.get_heatmap()
+        heatmap = self._arena_map.get_heatmap(self._map_side)
         heatmap = np.expand_dims(np.expand_dims(heatmap, axis=2), axis=0)
         ret['Learner'].trajectory_map = heatmap
         return ret
